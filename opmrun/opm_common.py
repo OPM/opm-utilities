8597fbbe413c94e13fd50d938e2c04d41b9b4564
OPMUSER <david.baxendale@gmail.com>
2020-03-01 15:21:03 +0800

    Added common utility functions used by other modules in OPMRUN in order to avoid code duplication.

--- opmrun/opm_common.py
+++ opmrun/opm_common.py
@@ -1,6 +1,6 @@
 # ======================================================================================================================
 #
-"""OPM-COMMON.py - Common Utility Functions
+"""OPM_COMMON.py - Common Utility Functions
 
 This module contains common utility functions used by other modules in OPMRUN in order to avoid code duplication.
 
@@ -31,24 +31,116 @@ Date    : 27-Feb-2020
 # ----------------------------------------------------------------------------------------------------------------------
 # Import Modules Section
 # ----------------------------------------------------------------------------------------------------------------------
+import os
 import sys
 #
 # Check for Python Version and Import Required Non-Standard Modules
 #
-if sys.version_info[0] >= 3:
-    try:
-        import PySimpleGUI as sg
-    except ImportError:
-        exit('OPMRUN Cannot Import PySimpleGUI - Please Install Using pip3')
+if sys.version_info[0] == 2:
+    exit('OPMRUN Only Works with Python 3, Python 2 Support is Depreciated')
 
-    try:
-        import tkinter as tk
-    except ImportError:
-        exit('OPMRUN Cannot Import tkinter - Please Install Using pip3')
+try:
+    import PySimpleGUI as sg
+except ImportError:
+    exit('OPMRUN Cannot Import PySimpleGUI - Please Install Using pip3')
+
+try:
+    from pathlib import Path
+except ImportError:
+    exit('OPMRUN Cannot Import Path from pathlib module - Please Install Using pip3')
+
+try:
+    import tkinter as tk
+except ImportError:
+    exit('OPMRUN Cannot Import tkinter - Please Install Using pip3')
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Define Modules Section
 # ----------------------------------------------------------------------------------------------------------------------
+def copy_to_clipboard(input):
+    """Copies Text to the Clipboard
+
+    Copies the input text to the clipboard for pasting into another application
+
+    Parameters
+    ----------
+    input: str
+        Input text to be copied to the clipboard
+
+    Returns
+    ------
+    None
+    """
+    #
+    # Define Tk Window and Prevent from Showing
+    #
+    root= tk.Tk()
+    root.withdraw()
+    #
+    # Clear Clipboard and Append Text
+    #
+    root.clipboard_clear()
+    root.clipboard_append(input)
+
+def opm_initialize():
+    """ Initialized OPMRUN
+
+    The function initializes OPMRUN environment and should be call for all sub-modules to ensure a consistent user
+    interface.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+
+    """
+
+    #
+    # OPMRUN ICON Base64 Encoded PNG File
+    #
+    opmicon  = Path(str(os.path.dirname( __file__ )) + '/opmrun.png')
+    if not os.path.isfile(opmicon):
+        sg.PopupTimed('Cannot Find ICON File opmrun.png - Program will Continue',
+                      no_titlebar=True, grab_anywhere=True, keep_on_top=True)
+        opmicon = None
+    #
+    # Set PySimpleGUI Defaults
+    #
+    sg.SetOptions(icon=opmicon,
+            button_color=('green','white'),
+            element_size=(None,None),
+            margins=(None,None),
+            element_padding=(None,None),
+            auto_size_text=None,
+            auto_size_buttons=None,
+            font=None,
+            border_width=1,
+            slider_border_width=None,
+            slider_relief=None,
+            slider_orientation=None,
+            autoclose_time=5,
+            message_box_line_width=None,
+            progress_meter_border_depth=None,
+            progress_meter_style=None,
+            progress_meter_relief=None,
+            progress_meter_color=None,
+            progress_meter_size=None,
+            text_justification=None,
+            text_color='black',
+            background_color='white',
+            element_background_color='white',
+            text_element_background_color='white',
+            input_elements_background_color=None ,
+            element_text_color='green',
+            input_text_color=None,
+            scrollbar_color=None,
+            debug_win_size=(None,None),
+            window_location=(None,None),
+            tooltip_time = None
+            )
 #
 # Define OPMRUN Modules for Stand Alone Running
 #
@@ -78,32 +170,6 @@ def opm_popup(opmvers, text,nrow):
     window1 = sg.Window('OPMRUN - Flow Job Scheduler ' + opmvers, layout=layout1)
     (button, values) = window1.Read()
     return()
-
-
-def copy_to_clipboard(input):
-    """Copies Text to the Clipboard
-
-    Copies the input text to the clipboard for pasting into another application
-
-    Parameters
-    ----------
-    input: str
-        Input text to be copied to the clipboard
-
-    Returns
-    ------
-    None
-    """
-    #
-    # Define Tk Window and Prevent from Showing
-    #
-    root= tk.Tk()
-    root.withdraw()
-    #
-    # Clear Clipboard and Append Text
-    #
-    root.clipboard_clear()
-    root.clipboard_append(input)
 
 # ======================================================================================================================
 # End of OPMKEYW
